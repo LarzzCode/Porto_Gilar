94449a8a342a61dece4d3d85ef3d8d2bf2922967
Gilar Wahiditya <115379190+LarzzCode@users.noreply.github.com>
2026-09-15 21:33:25 +0700

    feat: add services section to home

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Timeline from "./components/Timeline";
 import Projects from "./components/Projects";
+import Services from "./components/Services";
 import Certificates from "./components/Certificates";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -22,6 +23,7 @@ const Home = ({ showScrollBtn, scrollToTop }) => (
     <About />
     <Timeline />
     <Projects />
+    <Services />
     <Certificates />
     <Contact />
     <Footer />
